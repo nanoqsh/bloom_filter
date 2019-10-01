1de0360aa49c165a29b4c1feeea75a460e2f9bcf
nanonicholson <nanonicholson@gmail.com>
2019-10-02 02:36:57 +0600

    Update bitset

--- src/bitset.h
+++ src/bitset.h
@@ -17,11 +17,18 @@ public:
         size(size),
         array_size(ceil(size / 8.0)),
         bits(std::make_unique<uint8_t[]>(array_size))
-        {
+    {
         memset(bits.get(), 0, array_size);
     }
 
-    bitset(bitset& _) = delete;
+    bitset(bitset& other):
+        size(other.size),
+        array_size(other.array_size),
+        bits(std::make_unique<uint8_t[]>(array_size))
+    {
+        memcpy(bits.get(), other.bits.get(), array_size);
+    }
+
     bitset& operator=(bitset& _) = delete;
 
     void dump() {
@@ -32,12 +39,21 @@ public:
         for (size_t i = 0; i < array_size; ++i)
             cout << setfill('0') << setw(2) << hex << (int)bits[i] << ' ';
 
+        cout << '\n';
         cout.flags(defs);
     }
 
     void set(size_t pos) {
         position p = locate_bit(pos);
         bits[p.base] |= 1U << p.step;
+    }
+
+    void set(size_t pos, bool value) {
+        if (value) {
+            set(pos);
+        } else {
+            unset(pos);
+        }
     }
 
     void unset(size_t pos) {
--- src/main.cpp
+++ src/main.cpp
@@ -6,20 +6,33 @@
 int main() {
 
     bitset bits(45);
-    bits.set(8);
+    bits.set(8, true);
     bits.set(9);
     bits.set(10);
     bits.set(11);
-    bits.unset(11);
+    bits.set(11, false);
 
     assert(!bits.get(0));
     assert(!bits.get(1));
     assert(!bits.get(2));
     assert(!bits.get(3));
-    assert(bits.get(9));
-    assert(bits.get(10));
+    assert( bits.get(9));
+    assert( bits.get(10));
     assert(!bits.get(11));
     assert(!bits.get(12));
+
+    bitset bits2 = bits;
+    assert(!bits2.get(0));
+    assert(!bits2.get(1));
+    assert(!bits2.get(2));
+    assert(!bits2.get(3));
+    assert( bits2.get(9));
+    assert( bits2.get(10));
+    assert(!bits2.get(11));
+    assert(!bits2.get(12));
+
+    bits.dump();
+    bits2.dump();
 
     return 0;
 }
